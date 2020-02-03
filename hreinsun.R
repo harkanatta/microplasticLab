bb46739f49d63fced2b227ae5cd24b666d69bda7
Valtýr Sigurðsson <harkanatta@users.noreply.github.com>
2020-02-03 16:11:01 +0000

    barchart með sd error bars

--- hreinsun.R
+++ hreinsun.R
@@ -3,6 +3,8 @@
 library(dplyr)
 library(plyr)
 library(magrittr)
+library(DT)
+library(data.table)
 #data <- data %>% filter_all(any_vars(!is.na(.)))  # This is to get rid of the empty rows
 #
 #blank <- data[grep("lank",data$Sample),] # These are the blanks
@@ -13,7 +15,7 @@ library(magrittr)
 #print(data[grepl(" |,|[[:alpha:]]", data[,i]),i])
 #} #To check for commas in the variables of columns with numerical values. And to see if they aren't all only comprised of numbers but not alphabetical letters. Then I just fixe it in the .csv document.
 
-library(DT)
+
 # The column "Sample" should be divided into two columns: 'Station' with the station number, and 'Sample' with A, B, C etc.
 
 # The different names of colors can be simplified by having a column for the main color first and then more columns for the less prominent colors. A column with a binary could also be inserted to indicate whether the color is either striped or separated or if it's a blended color.
@@ -41,8 +43,7 @@ table(mp$Type)
 table(mp$Colour)
 table(blank$Type)
 
-library(DT)
-library(data.table)
+
 datatable(mp[order(mp[,6],decreasing = T),]) %>% 
   formatStyle(names(mp)[6],
               background = styleColorBar(range(mp[,6],na.rm = T), 'lightblue'),
@@ -91,3 +92,82 @@ table(data$Length)
 table(is.na(data$Length))
 
 
+
+
+
+
+
+
+
+
+
+library(RColorBrewer)
+gognin <- ddply(afli[afli$Skip=='Aðrir bátar' & afli$Ár==Árið,],.(Höfn,Mánuður = droplevels(Mánuður)),summarise,magn=sum(Magn),.drop=F)
+gognin <- droplevels(gognin)
+gat <- matrix(gognin$magn,nrow = length(levels(as.factor(gognin$Mánuður))),ncol = length(unique(gognin$Höfn)),dimnames=list(levels(as.factor(gognin$Mánuður)),levels(gognin$Höfn)))
+#colnames(gat) <- c('Norðurfjörður','Drangsnes','Hólmavík','Hvammstangi','Skagaströnd')
+colnames(gat) <- unique(gognin$Höfn)
+#barplot(gat,beside = T)
+
+litir <- colorRampPalette(c('#045579', 'white','#d75f07','seashell','#069acc'))(length(dimnames(gat)[[1]]))
+#png(filename="C:/Users/BioPol VS/Documents/Vinnumappa/landanir/NedriMynd.png",12,7,"cm",pointsize=6,res=900)
+png(filename="NedriMynd.png",12,7,"cm",pointsize=6,res=900)
+bp <- barplot(gat,ylab='',xlab='',axes=F,beside=TRUE,ylim=c(0,max(gognin$magn)*1.1))
+#grid(NA,ny = NULL)
+abline(h=seq(0,roundUpNice(1.1*max(gat)),roundUpNice(1.1*max(gat))/5), col = 'lightgray', lty = 3)
+barplot(gat, main=paste('Landanir í Húnaflóa',Árið,'-','Ferskt'), ylab="Magn (tonn)", beside=TRUE, axes = F, col=litir ,ylim = c(0,max(gognin$magn)*1.1),add=T);box()
+axis(2,seq(0,roundUpNice(1.1*max(gat)),roundUpNice(1.1*max(gat))/5),labels = seq(0,roundUpNice(1.1*max(gat))/1000,roundUpNice(1.1*max(gat))/5000),las=2)
+legend('topleft', levels(as.factor(gognin$Mánuður)), fill=litir)
+dev.off()
+
+matrix(DF$)
+
+
+data$blank <- ifelse(grepl("lank",data$Sample),"bl","mp")
+DF <- ddply(data,.(Station,blank),summarise,N=length(Sample))
+DF <- DF[-36,]
+DF <- tidyr::pivot_wider(DF,names_from = Station, values_from = N)
+DF <- DF[,-1]
+litir=c("#ff583f","#0032bc")
+png(filename="Barchart.png",12,7,"cm",pointsize=6,res=900)
+barplot(as.matrix(DF),beside = T,col = litir,ylab = "MP Particles",las=2)
+legend('topright',c("Samples","Blanks"),fill=litir)
+box()
+dev.off()
+
+DF$N <- ifelse(DF$blank=="bl",DF$N*(-1),DF$N*(1))
+DF <- ddply(DF,.(Station),summarise,N=sum(N))
+
+
+
+
+
+
+
+myData <- rbind(Mean,SD)
+myData <- as.data.frame(myData)
+
+par(mar = c(5, 6, 4, 5) + 0.1)
+
+myData[3,myData[1,]== max(myData[1,],na.rm = T) ]
+plotTop <- max(myData[1,],na.rm = T) +
+  myData[3,  myData[1,] == max(myData[1,],na.rm = T)] * 1.5
+
+barCenters <- barplot(as.matrix(Mean),
+                      beside = T, las = 2,
+                      ylim = c(0, plotTop), xaxt = "n",
+                      main = "Mileage by No. Cylinders and No. Gears",
+                      ylab = "Miles per Gallon",
+                      border = "black", axes = TRUE)
+
+# Specify the groupings. We use srt = 45 for a
+# 45 degree string rotation
+text(x = barCenters, y = par("usr")[3] - 1, srt = 45,
+     adj = 1, labels = myData$names, xpd = TRUE)
+
+segments(barCenters, as.matrix(Mean-SD) * 1, barCenters,
+         as.matrix(Mean+SD) * 1, lwd = 1.5)
+
+arrows(barCenters, as.matrix(Mean-SD) * 1, barCenters,
+       as.matrix(Mean+SD) * 1, lwd = 1.5, angle = 90,
+       code = 3, length = 0.05)
